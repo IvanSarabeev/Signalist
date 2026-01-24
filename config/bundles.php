45ccf4b4fa3ba47dd5c968d56a7b20c485ee2333
IvanSarabeev <ivan.sarabeev99@gmail.com>
2026-01-24 23:47:24 +0200

    remove jwt packages

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
