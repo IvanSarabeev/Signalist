713007071c14b51017ea8464a2591e3e61b42d41
IvanSarabeev <ivan.sarabeev99@gmail.com>
2026-02-21 19:29:49 +0200

    feat: added new RefreshTokens entity and doctrine migration

---
+++ migrations/Version20260221172648.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260221172648 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE refresh_tokens (id BIGINT AUTO_INCREMENT NOT NULL, user_id BIGINT NOT NULL, token VARCHAR(255) NOT NULL, expires_at DATETIME NOT NULL, created_at DATETIME NOT NULL, revoked TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_9BACE7E15F37A13B (token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE refresh_tokens');
+    }
+}
