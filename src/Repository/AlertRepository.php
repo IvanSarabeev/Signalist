683c2a9121bf417ad892fb8d485992e9e8b1a645
IvanSarabeev <ivan.sarabeev99@gmail.com>
2026-06-18 20:53:16 +0300

    refactor(alerts-list): refactor the returned response

--- src/Repository/AlertRepository.php
+++ src/Repository/AlertRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Alert;
 use App\Entity\User;
+use App\Presentation\Http\Response\Alerts\AlertItems;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Collections\Order;
 use Doctrine\ORM\AbstractQuery;
@@ -47,11 +48,27 @@ class AlertRepository extends ServiceEntityRepository
         }
 
         return array_map(
-            function(array $item) {
-                return [
-                    'id' => $item['id']
-                ];
-            },
+            fn(array $item) => new AlertItems(
+                id:                $item['id'],
+                alert_name:        $item['alertName'],
+                alert_type_label:  $item['alertType']->label(),
+                condition_quality: $item['conditionQuality']->value,
+                condition_label:   $item['conditionQuality']->label(),
+                condition_symbol:  $item['conditionQuality']->symbol(),
+                threshold_value:   $item['thresholdValue'],
+                frequency:         $item['frequency']->value,
+                frequency_label:   $item['frequency']->label(),
+                is_active:         $item['isActive'],
+                created_at:        $item['createdAt']?->format('Y-m-d H:i:s'),
+                last_triggered_at: $item['lastTriggeredAt']?->format('Y-m-d H:i:s'),
+                stock_symbol:      $item['stock']['symbol'] ?? null,
+                name:              $item['stock']['name'] ?? null,
+                price:             $item['stock']['cachedPrice'] ?? 0,
+                currency:          $item['stock']['currency'],
+                market_cap:        $item['stock']['cachedHigh'],
+                logo_url:          $item['stock']['logoUrl'],
+                change_percent:    $item['stock']['cachedChangePercent'],
+            ),
             $items
         );
     }
