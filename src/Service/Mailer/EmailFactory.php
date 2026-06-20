03883339ec977d3de5831443e7dfc864664318b7
IvanSarabeev <ivan.sarabeev99@gmail.com>
2026-06-20 23:43:12 +0300

    feat(email-factory): introduce new email methods

--- src/Service/Mailer/EmailFactory.php
+++ src/Service/Mailer/EmailFactory.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Mailer;
 
+use DateTime;
 use Symfony\Component\Mime\Email;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -55,6 +56,79 @@ final readonly class EmailFactory
                 $this->twig->render('emails/auth/welcome.html.twig', [
                     'name' => $name ?: 'Guest',
                     'intro' => 'Thanks for joining Signalist. You now have the tools to track markets and make smarter moves.'
+                ])
+            );
+    }
+
+
+    /**
+     * @param string $email
+     * @param string $symbol
+     * @param string $company
+     * @param float $currentMetric
+     * @param float $threshold
+     * @param string|null $triggeredAt
+     * @return Email
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createAlertUpperEmail(
+        string    $email,
+        string    $symbol,
+        string    $company,
+        float     $currentMetric,
+        float     $threshold,
+        ?string   $triggeredAt
+    ): Email
+    {
+        return (new Email())
+            ->from(self::FROM_MAIL)
+            ->to($email)
+            ->subject('Alert')
+            ->html(
+                $this->twig->render('emails/alerts/stocks/stock-alert-upper.html.twig', [
+                    'timestamp'    => $triggeredAt ?? new DateTime(),
+                    'symbol'       => $symbol,
+                    'company'      => $company,
+                    'currentPrice' => $currentMetric,
+                    'targetPrice'  => $threshold
+                ])
+            );
+    }
+
+    /**
+     * @param string $email
+     * @param string $symbol
+     * @param string $company
+     * @param float $currentMetric
+     * @param float $threshold
+     * @param string|null $triggeredAt
+     * @return Email
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createAlertLowerEmail(
+        string    $email,
+        string    $symbol,
+        string    $company,
+        float     $currentMetric,
+        float     $threshold,
+        ?string   $triggeredAt
+    ): Email
+    {
+        return (new Email())
+            ->from(self::FROM_MAIL)
+            ->to($email)
+            ->subject('Alert')
+            ->html(
+                $this->twig->render('emails/alerts/stocks/stock-alert-lower.html.twig', [
+                    'timestamp'    => $triggeredAt ?? new DateTime(),
+                    'symbol'       => $symbol,
+                    'company'      => $company,
+                    'currentPrice' => $currentMetric,
+                    'targetPrice'  => $threshold
                 ])
             );
     }
